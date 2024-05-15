d4634fa877585250124a33359f3a5f32586c8cd2
Nikolay Edigaryev <edigaryev@gmail.com>
2024-05-15 16:48:14 +0400

    Basic implementation (#2)
    * Basic implementation
    
    * Use `macos-runner:sonoma` instead of `macos-sonoma-xcode:latest`
    
    Co-authored-by: Fedor Korotkov <fedor.korotkov@gmail.com>
    
    * README.md: example with a version
    
    ---------
    
    Co-authored-by: Fedor Korotkov <fedor.korotkov@gmail.com>

---
+++ .github/fixtures/Test.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+// TODO: trigger linter warning
+print("Test!")
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,97 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import path from 'path'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+interface Entry {
+  file?: string
+  line?: number
+  character?: number
+  severity: string
+  type: string
+  rule_id: string
+  reason: string
+}
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const version: string = core.getInput('version')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Fail the workflow run if we're not running on macOS
+    if (process.platform !== 'darwin') {
+      core.setFailed(
+        `Unsupported OS "${process.platform}, only "darwin" is currently supported`
+      )
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      return
+    }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Determine the asset URL
+    let url: string
+
+    if (version !== 'latest') {
+      url = `https://github.com/realm/SwiftLint/releases/download/${version}/portable_swiftlint.zip`
+    } else {
+      url =
+        'https://github.com/realm/SwiftLint/releases/latest/download/portable_swiftlint.zip'
+    }
+
+    // Retrieve the SwiftLint binary from the tool cache
+    const toolName = `swiftlint-${process.platform}`
+    let portableSwiftlintDir = tc.find(toolName, version)
+
+    // Download the SwiftLint binary if not found in the tool cache
+    // or we're using the "latest" version
+    if (portableSwiftlintDir === '' || version === 'latest') {
+      const toolPath = await tc.downloadTool(url)
+      const toolDir = await tc.extractZip(toolPath)
+
+      // Cache the SwiftLint binary in the tool cache
+      portableSwiftlintDir = await tc.cacheDir(toolDir, toolName, version)
+    }
+
+    // Run the SwiftLint binary and capture its standard output
+    let stdout = ''
+
+    await exec.exec(
+      path.join(portableSwiftlintDir, 'swiftlint'),
+      ['lint', '--reporter=json'],
+      {
+        listeners: {
+          stdout: (data: Buffer) => {
+            stdout += data.toString()
+          }
+        }
+      }
+    )
+
+    // Parse the SwiftLint's JSON output
+    // and emit annotations
+    const result: Entry[] = JSON.parse(stdout)
+
+    for (const entry of result) {
+      let annotationFunc: (
+        message: string | Error,
+        properties?: core.AnnotationProperties
+      ) => void
+
+      switch (entry.severity.toLowerCase()) {
+        case 'warning':
+          annotationFunc = core.warning
+          break
+        case 'error':
+        default:
+          annotationFunc = core.error
+          break
+      }
+
+      annotationFunc(entry.reason, {
+        title: `${entry.type} (${entry.rule_id})`,
+        file: entry.file,
+        startLine: entry.line,
+        startColumn: entry.character
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
